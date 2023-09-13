c4d2168cea91e36356eba3cd1fc03af2d43a0177
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 16:03:37 +0300

    fix task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const refs = {
+    galleryList: document.querySelector('.js-gallery'),
+};
+
+
+
+function galleryMarkup(array) {
+  const markup = array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+
+  return markup;
+}
+
+function renderGalleryList(galleryArr) {
+  const markup = galleryMarkup(galleryArr);
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+renderGalleryList(galleryItems);
+
+
+new simpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
