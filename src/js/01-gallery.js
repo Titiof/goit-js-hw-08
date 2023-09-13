0339cf15a0f9211babd9fce57f81833b23c1a02a
Rumberg Vitalii <modtitiof@gmail.com>
2023-09-13 16:53:52 +0300

    clear

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 const refs = {
     galleryList: document.querySelector('.js-gallery'),
